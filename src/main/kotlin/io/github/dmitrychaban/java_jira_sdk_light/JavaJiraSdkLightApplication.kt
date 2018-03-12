eb5c1d6a3a86d228dc66acb86dfe52ddbcf98a8b
Dmitry Chaban <dmitrychaban@yahoo.com>
2018-03-12 08:53:36 +0200

    Initial commit

---
+++ src/main/kotlin/io/github/dmitrychaban/java_jira_sdk_light/JavaJiraSdkLightApplication.kt
@@ -0,0 +1,11 @@
+package io.github.dmitrychaban.java_jira_sdk_light
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JavaJiraSdkLightApplication
+
+fun main(args: Array<String>) {
+    runApplication<JavaJiraSdkLightApplication>(*args)
+}
---
+++ src/test/kotlin/io/github/dmitrychaban/java_jira_sdk_light/JavaJiraSdkLightApplicationTests.kt
@@ -0,0 +1,16 @@
+package io.github.dmitrychaban.java_jira_sdk_light
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class JavaJiraSdkLightApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
