5722cac38eae4da9b9fcb9f145448dbb9254d205
Dmitry Chaban <dmitrychaban@yahoo.com>
2018-03-14 12:09:30 +0200

    Added getTasksByProject api handling

--- src/main/kotlin/io/github/dmitrychaban/java_jira_sdk_light/DefaultJiraApi.kt
+++ src/main/kotlin/io/github/dmitrychaban/java_jira_sdk_light/DefaultJiraApi.kt
@@ -1,31 +1,33 @@
 package io.github.dmitrychaban.java_jira_sdk_light
 
 import io.github.dmitrychaban.java_jira_sdk_light.model.JiraProject
-import org.springframework.core.ParameterizedTypeReference
+import io.github.dmitrychaban.java_jira_sdk_light.model.QueryResponse
 import org.springframework.http.MediaType
 import org.springframework.web.reactive.function.client.WebClient
-import reactor.core.publisher.Flux
+import org.springframework.web.reactive.function.client.bodyToMono
 import reactor.core.publisher.Mono
 import java.util.*
 
 class DefaultJiraApi(val token: String, val mail: String) : JiraApi {
 
     val client: WebClient = WebClient.create("https://scenario.atlassian.net/rest/api/2")
+    val headerTokenValue = "$mail:$token"
+    val encodedHeaderTokenValue = String(Base64.getEncoder().encode(headerTokenValue.toByteArray()))
 
     override fun getJiraProjects(): Mono<List<JiraProject>> {
-        val headerTokenValue = "$mail:$token"
-        val encodedHeaderTokenValue = String(Base64.getEncoder().encode(headerTokenValue.toByteArray()))
-        val type = object : ParameterizedTypeReference<List<JiraProject>>() {}
         return client.get().uri("/project")
                 .accept(MediaType.APPLICATION_JSON)
                 .header("Authorization", "Basic $encodedHeaderTokenValue")
                 .retrieve()
-                .bodyToMono(type)
+                .bodyToMono()
+    }
+
+    override fun getTasksByProject(project: JiraProject, startFrom: Long, maxResult: Long): Mono<QueryResponse> {
+        return client.get().uri("/search?jql=project={projectId}", project.id)
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Authorization", "Basic $encodedHeaderTokenValue")
+                .retrieve()
+                .bodyToMono()
     }
 }
 
-
-fun main(args: Array<String>) {
-    var jiraProjects = DefaultJiraApi("", "").getJiraProjects()
-    println(jiraProjects.flatMapMany { Flux.fromIterable(it) }.map { it.name }.collectList().block())
-}
--- src/main/kotlin/io/github/dmitrychaban/java_jira_sdk_light/JiraApi.kt
+++ src/main/kotlin/io/github/dmitrychaban/java_jira_sdk_light/JiraApi.kt
@@ -1,10 +1,12 @@
 package io.github.dmitrychaban.java_jira_sdk_light
 
+import io.github.dmitrychaban.java_jira_sdk_light.model.Issue
 import io.github.dmitrychaban.java_jira_sdk_light.model.JiraProject
+import io.github.dmitrychaban.java_jira_sdk_light.model.QueryResponse
 import reactor.core.publisher.Mono
 
 interface JiraApi {
     fun getJiraProjects(): Mono<List<JiraProject>>
 
-//    fun getTasksByProject(project: JiraProject):
+    fun getTasksByProject(project: JiraProject, startFrom: Long, maxResult: Long): Mono<QueryResponse>
 }
